6add175b8cf6fbc7ee5adaf6e6c4491878b6bf6b
Your Name <bheemavathiboya127@gmail.com>
2022-11-22 17:08:12 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def second(request):
+    return HttpResponse('hi good afternoon')
+
+def third(request):
+     return HttpResponse('hi good afternoon')
